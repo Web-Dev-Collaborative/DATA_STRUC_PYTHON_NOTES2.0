d16d4a231e83b48fb95543adcdd9c8ee25b2195a
bryan <bryan.guner@gmail.com>
2021-08-17 07:10:29 -0400

    finished codesignal

--- WEEKS/wk19/d1/hw/mergeTwoLinkedLists.py
+++ WEEKS/wk19/d1/hw/mergeTwoLinkedLists.py
@@ -1,38 +1,39 @@
 # Note: Your solution should have O(l1.length + l2.length) time complexity, since this is what you will be asked to accomplish in an interview.
-
 # 
-
+# 
+# 
 # Example
-
+# 
 # For l1 = [1, 2, 3] and l2 = [4, 5, 6], the output should be
 # mergeTwoLinkedLists(l1, l2) = [1, 2, 3, 4, 5, 6];
 # For l1 = [1, 1, 2, 4] and l2 = [0, 3, 5], the output should be
 # mergeTwoLinkedLists(l1, l2) = [0, 1, 1, 2, 3, 4, 5].
 # Input/Output
-
+# 
 # [execution time limit] 4 seconds (py3)
-
+# 
 # [input] linkedlist.integer l1
-
+# 
 # A singly linked list of integers.
-
+# 
 # Guaranteed constraints:
 # 0 ≤ list size ≤ 104,
 # -109 ≤ element value ≤ 109.
-
+# 
 # [input] linkedlist.integer l2
-
+# 
 # A singly linked list of integers.
-
+# 
 # Guaranteed constraints:
 # 0 ≤ list size ≤ 104,
 # -109 ≤ element value ≤ 109.
-
+# 
 # [output] linkedlist.integer
-
+# 
 # A list that contains elements from both l1 and l2, sorted in non-decreasing order.
 
 
+#---------------------------------------------------------------------------------------------------
 # Singly-linked lists are already defined with this interface:
 # class ListNode(object):
 #   def __init__(self, x):
