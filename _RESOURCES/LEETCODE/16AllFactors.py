5fc4b170328869c98b5cbafd9b7639ebd8bcdca9
bryan <bryan.guner@gmail.com>
2021-09-09 04:32:30 -0400

    hi

---
+++ _RESOURCES/LEETCODE/16AllFactors.py
@@ -0,0 +1,8 @@
+#16. Write a Python Program to enter a number and display all its factors
+
+number = int(input("Enter the number"))
+
+for i in range(1, number+1):
+    if number % i == 0:
+        print(i)
+
